2444f0cf0f653e7410300e563ba4787750aa5f81
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-06 12:32:28 +0100

    Create register.mjs

---
+++ js/modules/api/authentication/register.mjs
@@ -0,0 +1,39 @@
+import { API_BASE_AUTH } from "../../constants/constants.mjs";
+
+const action = "register";
+const method = "POST";
+
+export async function register(profile) {
+    const url = API_BASE_AUTH+action;
+    const body = JSON.stringify(profile);
+
+    console.log(url);
+
+    const response = await fetch(url, {
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        method,
+        body,
+    })
+    
+    console.log(response);
+
+    const result = await response.json();
+    console.log(result);
+    if (response.status === 201) {
+        alert("Profile registered successfully!");
+    }else if (response.status === 400) {
+        alert("Profile already exists");
+    }else if(response.ok != true) {
+        alert("Something went wrong, please try again!")
+    }
+    return result;
+}
+
+const profileTrial = {
+    "name": "oleBraenna",
+    "email" : "OleBra17268@stud.noroff.no",
+    "password" : "1234abcd",
+};
+register(profileTrial);
