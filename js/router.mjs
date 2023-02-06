a9baad38bcafb9e67d8f2fd8956ba6d3707e58fc
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-06 13:21:09 +0100

    Update router.mjs

---
+++ js/router.mjs
@@ -0,0 +1,9 @@
+import * as listeners from "../js/modules/handlers/index.mjs";
+
+export function router() {
+    const path = location.pathname;
+    switch(path) {
+        case '/html/login/':
+            listeners.setLoginFormListener();
+    }
+}
