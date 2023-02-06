d11c03ed9d4cb084e70f6627c9bf7b33e5f8a2fa
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-06 12:36:54 +0100

    Create storage.mjs

---
+++ js/modules/storage/storage.mjs
@@ -0,0 +1,8 @@
+export function saveToLocal(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+};
+
+export function getFromLocal(key) {
+    const value = localStorage.getItem(key);
+    return JSON.parse(value);
+};
