ea51310ccad99e101ca1eebbe50ab4dc4aa137a2
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-21 13:51:24 +0100

    Create listings.mjs

---
+++ js/modules/api/profile/listings.mjs
@@ -0,0 +1,19 @@
+import * as storage from "../../storage/index.mjs";
+import { API_BASE_URL } from "../../constants/constants.mjs";
+import { authFetch } from "../listings/authFetch.mjs";
+
+export async function getProfileBids() {
+    const profile = storage.getFromLocal("profile");
+    const name = profile.name;
+    const URL = `${API_BASE_URL}profiles/${name}/bids?_listings=true`
+    const response = await authFetch(URL);
+    return await response.json();
+}
+
+export async function getProfileListings() {
+    const profile = storage.getFromLocal("profile");
+    const name = profile.name;
+    const URL = `${API_BASE_URL}profiles/${name}/listings`
+    const response = await authFetch(URL);
+    return await response.json();
+}
