bd5c989bc75769f892cf1dcef92bce83cec179b7
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-21 13:51:09 +0100

    Update index.mjs

--- js/index.mjs
+++ js/index.mjs
@@ -1,3 +1,26 @@
 import { router } from "../js/router.mjs";
+import * as templates from "../js/templates/index.mjs";
+import * as profile from "../js/modules/api/profile/index.mjs";
+import * as handlers from "../js/modules/handlers/index.mjs";
 
 router();
+
+async function profilePage() {
+    const profileInfo = await profile.getProfileInfo();
+    templates.profileInfoTemplate(profileInfo);
+}
+profilePage();
+
+async function profileListings() {
+    const listings = await profile.getProfileListings();
+    templates.profilePageListings(listings);
+}
+profileListings();
+
+async function profileBids() {
+    const bids = await profile.getProfileBids();
+    templates.profilePageBids(bids);
+}
+profileBids();
+
+handlers.setUpdateAvatarListener();
