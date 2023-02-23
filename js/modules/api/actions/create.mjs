9c35639bdd8507b7e5e14a2517dc3915e118ea37
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-23 15:16:08 +0100

    Create create.mjs

---
+++ js/modules/api/actions/create.mjs
@@ -0,0 +1,26 @@
+import { API_BASE_URL } from "../../constants/constants.mjs";
+import { authFetch } from "../../api/actions/authFetch.mjs";
+
+const action = "listings";
+const method = "POST";
+
+const trialListing = {
+    "title" : "testuser911-listing",
+    "endsAt" : "2023-02-17T00:00:00.000Z"
+};
+
+export async function createListing(listingData) {
+    const createListingURL = `${API_BASE_URL}${action}`;
+
+    const response = await authFetch(createListingURL, {
+        method,
+        body: JSON.stringify(listingData)
+    });
+    const result = await response.json();
+    if (response.ok === true) {
+        alert("Listing created!")
+    }
+    console.log(response);
+    console.log(result);
+    return result;
+}
