68e1b86fe0242dd0f61da0cedf54727131810726
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-23 15:16:38 +0100

    Update index.mjs

--- js/modules/handlers/index.mjs
+++ js/modules/handlers/index.mjs
@@ -2,3 +2,5 @@ export * from "../handlers/login.mjs";
 export * from "../handlers/register.mjs";
 export * from "../handlers/update.mjs";
 export * from "../handlers/create.mjs";
+export * from "../handlers/bid.mjs";
+export * from "../handlers/logout.mjs";
