d2983609b16782707d5f4e4628e9fd42f59a6883
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-23 15:16:36 +0100

    Update create.mjs

--- js/modules/handlers/create.mjs
+++ js/modules/handlers/create.mjs
@@ -1,4 +1,4 @@
-import { createListing } from "../api/listings/create.mjs";
+import { createListing } from "../api/actions/create.mjs";
 
 export async function setCreateListingListener() {
     const form = document.querySelector("#createListingForm");
@@ -9,6 +9,12 @@ export async function setCreateListingListener() {
             const listingForm = event.target;
             const formData = new FormData(listingForm);
             const listingData = Object.fromEntries(formData.entries());
+            const month = document.querySelector("#createMonth");
+            const day = document.querySelector("#createDay");
+            const hour = document.querySelector("#createHour");
+            const minute = document.querySelector("#createMinute");
+            const endsAt = `2023-${month.value}-${day.value}T${hour.value}:${minute.value}:00.000Z`
+            console.log(endsAt);
             const media = [];
             if(listingData.media1.length >= 1) {
                 media.push(listingData.media1);
@@ -27,6 +33,7 @@ export async function setCreateListingListener() {
             }
             const objectToList = {
                 "media" : media,
+                "endsAt" : endsAt,
                 ...listingData
             }
             createListing(objectToList);
