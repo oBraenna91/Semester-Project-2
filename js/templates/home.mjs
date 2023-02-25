e067f24713f3d50ce86a5d647bd261355190bbbd
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-25 18:40:15 +0100

    Update home.mjs

--- js/templates/home.mjs
+++ js/templates/home.mjs
@@ -5,18 +5,15 @@ export async function homePageListings(listings) {
         return listings.forEach(function(listing){
             homePageListingContainer.innerHTML +=
         `
-        <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-heading${[listing.id]}">
-                    <button class="accordion-button bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${[listing.id]}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${[listing.id]}">
-                        <strong>${listing.title}</strong>
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapse${[listing.id]}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading${[listing.id]}">
-                    <div class="accordion-body bg-secondary text-white">
-                        <p>${listing.description}</p>
-                        <a href="/listing/index.html?id=${listing.id}"> Click to view more </a>
-                    </div>
+        <div class="card my-4 m-auto" style="width: 50%">
+            <img src="${listing.media[0]}" class="card-img-top img-fluid" alt="alt">
+            <div class="card-body justify-content-center">
+                <h5 class="card-title text-center">${listing.title}</h5>
+                <p class="card-text text-center">${listing.description}</p>
+                <div class="text-center">
+                    <a href="/listing/index.html?id=${listing.id}" class="btn btn-primary">See more</a>
                 </div>
+            </div>
         </div>
         `;
         })
