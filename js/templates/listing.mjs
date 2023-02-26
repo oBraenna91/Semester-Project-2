a0d667bf64f8977f75e7276478a96e845f66349b
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-26 10:43:33 +0100

    Update listing.mjs

--- js/templates/listing.mjs
+++ js/templates/listing.mjs
@@ -1,18 +1,43 @@
 const resultsContainer = document.querySelector("#listingPageContainer");
 const bidContainer = document.querySelector("#bidContainer");
 const imageContainer = document.querySelector("#imageContainer");
+const timerContainer = document.querySelector("#timerContainer");
 
 export async function listingPageTemplate(listing) {
+
     if(resultsContainer) {
         return resultsContainer.innerHTML +=
         `
             <h3>Title: ${listing.title}</h3>
             <p>Description: ${listing.description}</p>
-            <p>Listing ends at: ${listing.endsAt}</p>
             
         `;
     }
 }
+
+export function listingTimerTemplate(listing) {
+    if (timerContainer){
+        const timer = setInterval(function() {
+            const date = new Date(listing.endsAt);
+            const now = new Date().getTime();
+        
+            const timeToEnd = date - now;
+            
+            const days = Math.floor(timeToEnd / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeToEnd % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeToEnd % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeToEnd % (1000 * 60)) / 1000);
+
+            timerContainer.innerHTML = 
+            `
+            <p>${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds</p>
+            `
+        //console.log(days, "days", hours, "hours", minutes, "minutes", seconds, "seconds");
+        },
+        1000);
+    }
+}
+        
 
 export async function listingPageBids(bidData) {
     return bidData.forEach(function(bid){
