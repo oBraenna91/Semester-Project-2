fd717251caf101084aff3d57ffa5bf60c2313f1a
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-26 10:43:39 +0100

    Update router.mjs

--- js/router.mjs
+++ js/router.mjs
@@ -46,10 +46,10 @@ export function router() {
                 const post = await actions.getListing(id);
                 templates.listingPageTemplate(post);
                 const bids = post.bids;
-                console.log(bids);
                 const media = post.media;
                 templates.listingPageBids(bids);
                 templates.listingPageImages(media);
+                templates.listingTimerTemplate(post);
             }
             listingPage();
             handlers.setBidOnListingHandler();
