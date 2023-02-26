9310143204de9508981b17aaa273af4a1b8b63f4
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-26 09:05:18 +0100

    Update profile.mjs

--- js/templates/profile.mjs
+++ js/templates/profile.mjs
@@ -5,10 +5,10 @@ const bidsContainer = document.querySelector("#profile-bids");
 export async function profileInfoTemplate(user) {
     if(infoContainer) {
         return infoContainer.innerHTML +=
-        `   <div class="col-8">
+        `   <div class="col-8 m-auto">
                 <img src="${user.avatar} class="img-fluid" style="width:100%">
             </div>
-            <div class="">
+            <div class="text-center">
                 <h2> Name - ${user.name} </h2>
                 <h2> Credits - ${user.credits} </h2>
             </div>
@@ -21,18 +21,15 @@ export async function profilePageListings(listings) {
         return listings.forEach(function(listing){
             listingContainer.innerHTML +=
         `
-        <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-heading${[listing.id]}">
-                    <button class="accordion-button bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${[listing.id]}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${[listing.id]}">
-                        <strong>${listing.title}</strong>
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapse${[listing.id]}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading${[listing.id]}">
-                    <div class="accordion-body bg-secondary text-white">
-                        <p>${listing.description}</p>
-                        <a href="/listing/index.html?id=${listing.id}"> Click to view more </a>
-                    </div>
+        <div class="card my-4 m-auto" style="width: 50%">
+            <img src="${listing.media[0]}" class="card-img-top img-fluid" alt="alt">
+            <div class="card-body justify-content-center">
+                <h5 class="card-title text-center">${listing.title}</h5>
+                <p class="card-text text-center">${listing.description}</p>
+                <div class="text-center">
+                    <a href="/listing/index.html?id=${listing.id}" class="btn btn-primary">See more</a>
                 </div>
+            </div>
         </div>
         `;
         })
@@ -44,18 +41,15 @@ export async function profilePageBids(listings) {
         return listings.forEach(function(listing){
             bidsContainer.innerHTML +=
         `
-        <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-heading${[listing.id]}">
-                    <button class="accordion-button bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${[listing.id]}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${[listing.id]}">
-                        <strong>${listing['listing'].title}</strong>
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapse${[listing.id]}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading${[listing.id]}">
-                    <div class="accordion-body bg-secondary text-white">
-                        <p>${listing['listing'].description}</p>
-                        <a href="/listing/index.html?id=${listing.id}"> Click to view more </a>
-                    </div>
+        <div class="card my-4 m-auto" style="width: 50%">
+            <img src="${listing['listing'].media[0]}" class="card-img-top img-fluid" alt="alt">
+            <div class="card-body justify-content-center">
+                <h5 class="card-title text-center">${listing['listing'].title}</h5>
+                <p class="card-text text-center">${listing['listing'].description}</p>
+                <div class="text-center">
+                    <a href="/listing/index.html?id=${listing.id}" class="btn btn-primary">See more</a>
                 </div>
+            </div>
         </div>
         `;
         })
