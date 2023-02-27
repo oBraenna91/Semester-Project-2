fd8dd7751c8d8a9cf61b61dc971b65c96c3fea88
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-27 13:20:39 +0100

    Update home.mjs

--- js/templates/home.mjs
+++ js/templates/home.mjs
@@ -6,10 +6,9 @@ export async function homePageListings(listings) {
             homePageListingContainer.innerHTML +=
         `
         <div class="card my-4 m-auto" style="width: 50%">
-            <img src="${listing.media[0]}" class="card-img-top img-fluid" alt="alt">
+            <img src="${listing.media[0]}" class="card-img-top img-fluid" alt="No image provided">
             <div class="card-body justify-content-center">
                 <h5 class="card-title text-center">${listing.title}</h5>
-                <p class="card-text text-center">${listing.description}</p>
                 <div class="text-center">
                     <a href="/listing/index.html?id=${listing.id}" class="btn btn-primary">See more</a>
                 </div>
