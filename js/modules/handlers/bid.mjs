e44b83003e69e9ed5bcc50b18c74ef1bea61eea8
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-27 13:21:02 +0100

    Update bid.mjs

--- js/modules/handlers/bid.mjs
+++ js/modules/handlers/bid.mjs
@@ -1,9 +1,9 @@
 import { bidOnListing } from "../api/actions/bid.mjs";
 import { extractID } from "../api/actions/extract.mjs";
+import * as storage from "../storage/index.mjs";
 
 export async function setBidOnListingHandler() {
-
-    const form = document.querySelector("#makeABidForm");
+        const form = document.querySelector("#makeABidForm");
 
     if(form) {
         form.addEventListener("submit", (event) => {
@@ -16,7 +16,12 @@ export async function setBidOnListingHandler() {
             const amountToSend = {
                 "amount" : newAmount
             }
+            if(storage.getFromLocal("accessToken")) {
             bidOnListing(id, amountToSend);
+            }else{
+                alert("You are not logged in! Please log in to bid on this auction")
+                window.location.href="/login/"
+            }
         })
     }
 }
