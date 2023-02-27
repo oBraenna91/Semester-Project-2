c0cc921f43d56228239aa51ee39e47eb7ac3123b
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-02-27 13:21:09 +0100

    Update profile.mjs

--- js/templates/profile.mjs
+++ js/templates/profile.mjs
@@ -5,12 +5,12 @@ const bidsContainer = document.querySelector("#profile-bids");
 export async function profileInfoTemplate(user) {
     if(infoContainer) {
         return infoContainer.innerHTML +=
-        `   <div class="col-8 m-auto">
+        `   <div class="col-3 col-md-2 m-auto">
                 <img src="${user.avatar} class="img-fluid" style="width:100%">
             </div>
-            <div class="text-center">
-                <h2> Name - ${user.name} </h2>
-                <h2> Credits - ${user.credits} </h2>
+            <div class="d-flex-column justify-content-center align-self-center col-8 text-center mt-md-3">
+                <h2> Name: ${user.name} </h2>
+                <h2> Credits: ${user.credits} </h2>
             </div>
         `
     }
@@ -25,7 +25,6 @@ export async function profilePageListings(listings) {
             <img src="${listing.media[0]}" class="card-img-top img-fluid" alt="alt">
             <div class="card-body justify-content-center">
                 <h5 class="card-title text-center">${listing.title}</h5>
-                <p class="card-text text-center">${listing.description}</p>
                 <div class="text-center">
                     <a href="/listing/index.html?id=${listing.id}" class="btn btn-primary">See more</a>
                 </div>
@@ -45,7 +44,6 @@ export async function profilePageBids(listings) {
             <img src="${listing['listing'].media[0]}" class="card-img-top img-fluid" alt="alt">
             <div class="card-body justify-content-center">
                 <h5 class="card-title text-center">${listing['listing'].title}</h5>
-                <p class="card-text text-center">${listing['listing'].description}</p>
                 <div class="text-center">
                     <a href="/listing/index.html?id=${listing.id}" class="btn btn-primary">See more</a>
                 </div>
