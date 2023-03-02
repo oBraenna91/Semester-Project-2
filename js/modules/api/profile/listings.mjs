8257ad722b5f8f6589e47892ae7c6f91bdb6d9ee
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-03-02 09:45:56 +0100

    Update listings.mjs

--- js/modules/api/profile/listings.mjs
+++ js/modules/api/profile/listings.mjs
@@ -2,6 +2,11 @@ import * as storage from "../../storage/index.mjs";
 import { API_BASE_URL } from "../../constants/constants.mjs";
 import { authFetch } from "../actions/authFetch.mjs";
 
+/**
+ * This function retrieves all bids made by a user from the API, using the logged in users name
+ * retrieved from local storage.
+ * @returns an array with the bids.
+ */
 export async function getProfileBids() {
     const profile = storage.getFromLocal("profile");
     const name = profile.name;
@@ -10,6 +15,10 @@ export async function getProfileBids() {
     return await response.json();
 }
 
+/**
+ * This function retrieves all active listings from the API for the profile of the logged in user.
+ * @returns the active listings in an array.
+ */
 export async function getProfileListings() {
     const profile = storage.getFromLocal("profile");
     const name = profile.name;
