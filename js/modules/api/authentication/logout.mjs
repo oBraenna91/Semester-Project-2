4902b3eab374a97831b266a218f1240eb7a4d50d
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-03-02 09:47:12 +0100

    Update logout.mjs

--- js/modules/api/authentication/logout.mjs
+++ js/modules/api/authentication/logout.mjs
@@ -1,5 +1,8 @@
 import * as storage from "../../storage/index.mjs";
 
+/**
+ * This function removes the profile information and accessToken from localStorage.
+ */
 export function logOut(){
     storage.removeFromLocal("profile");
     storage.removeFromLocal("accessToken");
