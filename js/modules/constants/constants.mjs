5a3d59f49c84b48650a3f26e3e5fed8558819006
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-03-02 09:46:22 +0100

    Update constants.mjs

--- js/modules/constants/constants.mjs
+++ js/modules/constants/constants.mjs
@@ -1,3 +1,9 @@
+/**
+ * This const contains the API base URL for all the unauthorized endpoints.
+ */
 export const API_BASE_URL = "https://api.noroff.dev/api/v1/auction/";
 
+/**
+ * This const contains the API base URL for all the authorized endpoints. 
+ */
 export const API_BASE_AUTH = `${API_BASE_URL}auth/`;
