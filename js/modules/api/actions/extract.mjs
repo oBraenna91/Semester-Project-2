8c87ef2a4454898919673791c684a4a44ba3f72a
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-03-02 09:47:04 +0100

    Update extract.mjs

--- js/modules/api/actions/extract.mjs
+++ js/modules/api/actions/extract.mjs
@@ -1,3 +1,8 @@
+/**
+ * This function extracts the listing-ID from the URL.
+ * @returns the listing-ID.
+ */
+
 export function extractID() {
     const queryString = document.location.search;
     const params = new URLSearchParams (queryString);
