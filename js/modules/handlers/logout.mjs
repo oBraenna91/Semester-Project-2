18619a9516438618904722f83c1f0bcfa2db54c7
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-03-02 09:46:04 +0100

    Update logout.mjs

--- js/modules/handlers/logout.mjs
+++ js/modules/handlers/logout.mjs
@@ -1,5 +1,10 @@
 import { logOut } from "../api/authentication/logout.mjs"; 
 
+/**
+ * This function sets an addEventListener to the logout-button and
+ * logs the user out using the logOut function and alers the user that
+ * they are successfully logged out.
+ */
 export function setLogoutFormListener() {
     const button = document.querySelector("#logout-button");
 
