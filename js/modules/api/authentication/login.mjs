e54fc8fed29c9dfccb575b526d78a926998ea4d8
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-03-03 15:03:32 +0100

    Update login.mjs

--- js/modules/api/authentication/login.mjs
+++ js/modules/api/authentication/login.mjs
@@ -23,10 +23,13 @@ export async function login(profile) {
         body,
     })
 
-    const { accessToken, ...user } = await response.json();
+
+    const result = await response.json();
+    const { accessToken, ...user } = result;
 
     storage.saveToLocal("accessToken", accessToken);
     storage.saveToLocal("profile", user);
+    
     if (response.status === 200) {
         alert("You are now logged in!");
         window.location.href="/profile";
